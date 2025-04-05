0984417f3eb0400122ff48eade9cbaca649714df
Baptiste <pereirabaptiste1@gmail.com>
2025-04-05 23:59:00 +0200

    Feat: Création de la migraiton de la table people

---
+++ database/migrations/2025_04_05_215747_create_people_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('created_by');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('birth_name')->nullable();
+            $table->string('middle_names')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->index('created_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
